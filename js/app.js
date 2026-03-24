a6278bc689e781a8f4084c58cb705f68a5cf19b8
wald52 <wald52@protonmail.com>
2026-03-24 01:19:57 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 import { init as bootstrapApp } from './app-runtime.js';
+import { createPwaController } from './ui/pwa-controller.js';
 
+const pwaController = createPwaController();
+
+void pwaController.init();
 bootstrapApp();
 
-export { bootstrapApp };
+export { bootstrapApp, pwaController };
 export default bootstrapApp;
